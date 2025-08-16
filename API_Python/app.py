9c9f397ff73d7d48639b66dbc14452b45ddf2e66
Jose Palacio <josepalacio145@gmail.com>
2025-08-16 13:10:08 -0500

    " se cambio archivo pyton en el arreglo de familia, por la mia"

--- API_Python/app.py
+++ API_Python/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/familia")
 def get_familia():
-    rows = ["Amin", "Marce", "Miranda"]
+    rows = ["Leidy", "Abigail", "Leticia","Jacque"]
     return rows
 
 @app.get("/superheroesDC")
